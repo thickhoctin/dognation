671d04bf64a4ef8e11ef5371b4f6a0750cf0d0c3
Thanh Tung Nguyen <thickhoctin@gmail.com>
2025-05-11 23:46:05 +0200

    pass

---
+++ src/passport.js
@@ -0,0 +1,10 @@
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcrypt");
+const helper = require("../helpers/helper");
+
+// Set up the Passport strategy:
+
+// Serialize a user
+
+// Deserialize a user
